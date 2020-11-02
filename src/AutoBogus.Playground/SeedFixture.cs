b50aa6bd86ed49acdd803cc29f1ec42e0e9a3250
Nick Dodd <nick@honeag.com>
2020-11-02 19:34:38 +1100

    Added seed tests

---
+++ src/AutoBogus.Playground/SeedFixture.cs
@@ -0,0 +1,70 @@
+using Bogus;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AutoBogus.Playground
+{
+  public class SeedFixture
+  {
+    private sealed class MyEntity
+    {
+      public DateTimeOffset DeprecationDate { get; set; }
+      public string Name { get; set; }
+    }
+
+    public class Book
+    {
+      public int Id { get; set; }
+      public string Name { get; set; }
+    }
+
+    public class Author
+    {
+      public int Id { get; set; }
+      public string FirstName { get; set; }
+      public string LastName { get; set; }
+
+      // Navigation properties
+      public ICollection<Book> Books { get; set; }
+    }
+
+    [Fact]
+    public void DateTimeOffsetTest()
+    {
+      int seed = 1;
+
+      var faker1 = new AutoFaker<MyEntity>().UseSeed(seed);
+      var entity1 = faker1.Generate();
+            
+      var faker2 = new AutoFaker<MyEntity>().UseSeed(seed);
+      var entity2 = faker2.Generate();
+
+      entity2.Name.Should().Be(entity1.Name); 
+      entity2.DeprecationDate.Should().BeCloseTo(entity1.DeprecationDate, 50);
+    }
+
+    [Fact]
+    public void AuthorTest()
+    {
+      // Global seed configuration
+      Randomizer.Seed = new Random(8675309);
+
+      // Local seed configuration
+      const int seed = 1234;
+      var authorFaker = new AutoFaker<Author>()
+        .Configure(builder => builder
+          .WithSkip<Author>(a => a.Id)
+          .WithSkip<Author>(a => a.Books))
+        .UseSeed(seed);
+
+      // Check output using LINQPad
+      var author = authorFaker.Generate();
+
+      author.FirstName.Should().Be("collaborative");
+      author.LastName.Should().Be("haptic");
+    }
+  }
+}
